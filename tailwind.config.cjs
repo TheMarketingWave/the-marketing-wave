99481a9e5a7289a0cfa4426dbcca93bcbea87375
StefanRadusi <radusi.stefan@gmail.com>
2024-10-30 12:20:40 +0200

    update font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat"],
+        montserrat: ['"Montserrat Alternates"'],
       },
       colors: {
         "brand-orange": "#FA4415",
